14fa8fc911b77209362413ba7cbb49a9339f66f0
Rowan <malice.peanutbutter@gmail.com>
2024-07-02 21:17:59 +0200

    Mspaint is now a module. Lots of module mechanism stuff.

--- README.md
+++ README.md
@@ -2,7 +2,18 @@ This is really nothing right now.
 
 I intend for it to be a cool collection of silly animation stuff.
 
-todo: 
+
+# What is a module? 
+
+A module is a folder that specifically contains the following: 
+  `index.html` - generally a file that have a div for whatever you want your script and styles to be loaded into.
+  `script.js` - the javascript that is loaded into the index.html file. It's important to IIFE-ify this file. 
+  `style.css` - the css that is loaded into the index.html file.
+
+*There will soon be a cool automatic way to scaffold this*
+
+
+# todo: 
 
 - Have all the different movements exist in the same space. (Currently there are 3 different things)
 - Have a way to switch between the different sub movements
--- script.js
+++ script.js
@@ -2,20 +2,60 @@ document.addEventListener("DOMContentLoaded", () => {
   const buttons = document.querySelectorAll(".button-container button");
   const moduleContent = document.getElementById("module-content");
 
+  const loadModule = async (moduleName) => {
+    moduleContent.innerHTML = "";
+
+    const existingScript = document.getElementById("module-script");
+    if (existingScript) {
+      existingScript.remove();
+    }
+    const existingLink = document.getElementById("module-style");
+    if (existingLink) {
+      existingLink.remove();
+    }
+
+    const html = await fetch(`modules/${moduleName}/index.html`).then((res) =>
+      res.text()
+    );
+    moduleContent.innerHTML = html;
+
+    const script = document.createElement("script");
+    script.src = `modules/${moduleName}/script.js`;
+    script.id = "module-script";
+    moduleContent.appendChild(script);
+
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = `modules/${moduleName}/style.css`;
+    link.id = "module-style";
+    document.head.appendChild(link);
+
+    buttons.forEach((button) => {
+      button.classList.remove("active");
+      if (button.getAttribute("data-module") === moduleName) {
+        button.classList.add("active");
+      }
+    });
+
+    history.pushState({ module: moduleName }, "", `?m=${moduleName}`);
+  };
+
   buttons.forEach((button) => {
-    button.addEventListener("click", async () => {
+    button.addEventListener("click", () => {
       const moduleName = button.getAttribute("data-module");
-      const html = await fetch(`modules/${moduleName}/index.html`).then((res) =>
-        res.text()
-      );
-      moduleContent.innerHTML = html;
-      const script = document.createElement("script");
-      script.src = `modules/${moduleName}/script.js`;
-      moduleContent.appendChild(script);
-      const link = document.createElement("link");
-      link.rel = "stylesheet";
-      link.href = `modules/${moduleName}/style.css`;
-      document.head.appendChild(link);
+      loadModule(moduleName);
     });
   });
+
+  window.addEventListener("popstate", (event) => {
+    if (event.state && event.state.module) {
+      loadModule(event.state.module);
+    }
+  });
+
+  const urlParams = new URLSearchParams(window.location.search);
+  const initialModule = urlParams.get("m");
+  if (initialModule) {
+    loadModule(initialModule);
+  }
 });
